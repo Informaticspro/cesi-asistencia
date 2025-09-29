b9a94b18d42d305e7f35892016f1370fa4f5f346
jose acosta <jose>
2025-09-28 22:43:37 -0500

    prueba final 1

--- src/RegistrarParticipante.jsx
+++ src/RegistrarParticipante.jsx
@@ -125,7 +125,7 @@ setMostrarErrorCedula(false);
   const response = await fetch(`${import.meta.env.VITE_API_URL}/registro`, {
   method: "POST",
   headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ cedula, nombre, correo }),
+  body: JSON.stringify({ cedula, nombre, correo, categoria, modalidad }),
 });
 
     if (!response.ok) {
